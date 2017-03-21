e8a1e35873967c46c0654b5852e8c3bad6e90130
angeldslee <angeldslee@gmail.com>
2017-03-21 23:26:52 +0800

    openid providers

--- app/views.py
+++ app/views.py
@@ -4,8 +4,6 @@ from .forms import LoginForm
 
 @app.route('/')
 @app.route('/index')
-@app.route('/login', methods=['get', 'post'])
-
 def index():
     user = {'nickname': 'dsli'}
     posts = [  # fake array of posts
@@ -20,6 +18,7 @@ def index():
     ]
     return render_template('index.html', title='home', user=user, posts=posts)
 
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
@@ -27,4 +26,5 @@ def login():
         return redirect('/index')
     return render_template('login.html', 
                           title="Sing in", 
-                          form=form)
+                          form=form,
+                          providers=app.config['OPENID_PROVIDERS'])
