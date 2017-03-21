f9aac57aa96f8fd1117c479781ec4aa0e1c943eb
angeldslee <angeldslee@gmail.com>
2017-03-21 15:16:31 +0800

    add form reaction

--- app/views.py
+++ app/views.py
@@ -2,25 +2,29 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
-# @app.route('/')
-# @app.route('/index')
+@app.route('/')
+@app.route('/index')
 @app.route('/login', methods=['get', 'post'])
 
-# def index():
-#    # return "hello, dsli!"
-#    user = {'nickname': 'dsli'}
-#    posts = [  # fake array of posts
-#         { 
-#             'author': {'nickname': 'John'}, 
-#             'body': 'Beautiful day in Portland!' 
-#         },
-#         { 
-#             'author': {'nickname': 'Susan'}, 
-#             'body': 'The Avengers movie was so cool!' 
-#         }
-#     ]
-#    return render_template('index.html', title='home', user=user, posts=posts
+def index():
+    user = {'nickname': 'dsli'}
+    posts = [  # fake array of posts
+        { 
+            'author': {'nickname': 'John'}, 
+            'body': 'Beautiful day in Portland!' 
+        },
+        { 
+            'author': {'nickname': 'Susan'}, 
+            'body': 'The Avengers movie was so cool!' 
+        }
+    ]
+    return render_template('index.html', title='home', user=user, posts=posts)
 
 def login():
-  form = LoginForm()
-  return render_template('login.html', title="Sing in", form=form)
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('login requested for openid=%s, remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html', 
+                          title="Sing in", 
+                          form=form)
